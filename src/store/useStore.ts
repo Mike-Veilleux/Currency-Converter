840b334fd355e6670610068064acdee56f0f6308
Michael Veilleux <>
2023-02-24 19:37:27 +0800

    Update useStore.ts

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -31,7 +31,7 @@ const useStore = create<dataStore>((set, get) => ({
   selectedCurrency: ["btc", "Bitcoin"],
   currencyCode: "btc",
   amountToConvert: 1,
-  favoriteRates: [],
+  favoriteRates: ["usd"],
   // favoriteRates: ["usd", "cad", "hkd", "eur", "gbp"],
   actions: {
     fetchDefinitions: async () => {
